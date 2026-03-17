7c7a250e0ecfbf30e78d7fb76720688a68056f3d
silencecraft1-star <silencecraft1@gmail.com>
2026-03-17 21:25:24 +0700

    update

--- resources/views/penilaian/score_new.blade.php
+++ resources/views/penilaian/score_new.blade.php
@@ -157,7 +157,7 @@
     <section>
         <section>
             <nav class=" w-full z-20 top-0 px-0 start-0 border-b border-gray-200 dark:border-gray-600 bg-white"
-                style="color: #F5F5F5; background: linear-gradient(to right, #2e2e2eff, #a7a7a7ff, #2e2e2eff);">
+                style="color: #F5F5F5; background: linear-gradient(to right, #707070ff, #a7a7a7ff, #707070ff);">
                 <div id="idpartai" name="{{ $partai }}" class="grid grid-cols-3 " style="padding: 0px 8vh;">
                     <!-- Judul Turnamen -->
                     <div class="text-neutral-200 text-white font-semibold text-start w-full h-full flex justify-start items-center"
