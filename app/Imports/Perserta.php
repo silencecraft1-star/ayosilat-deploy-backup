8867abafda3dd4db70a3f54160b794fe4008df87
silencecraft1-star <silencecraft1@gmail.com>
2026-04-19 21:05:35 +0700

    struktur import perserta

--- app/Imports/Perserta.php
+++ app/Imports/Perserta.php
@@ -12,59 +12,59 @@ use App\KontigenModel;
 class Perserta implements ToCollection
 {
     /**
-    * @param Collection $collection
-    */
+     * @param Collection $collection
+     */
     public function collection(Collection $collection)
     {
         $data = $collection->slice(1);
-        foreach($data as $item){
-            if (empty($item[1])) {
+        foreach ($data as $item) {
+            if (empty($item[0])) {
                 continue;
             }
             //dd($data);
-            $name = $item[1];
-            $gender = $item[3];
-            $kontigen = $item[2];
-            $kelas = $item[5];
-            $category = $item[4];
-            $check_kelas = kelas::where('name',$kelas)->first();
-            if(empty($check_kelas)){
+            $name = $item[0];
+            $gender = $item[2];
+            $kontigen = $item[1];
+            $kelas = $item[4];
+            $category = $item[3];
+            $check_kelas = kelas::where('name', $kelas)->first();
+            if (empty($check_kelas)) {
                 kelas::create(
                     [
-                        'name'=>$kelas
+                        'name' => $kelas
                     ]
                 );
             }
-            $id_kelas = kelas::where('name',$kelas)->value('id');
+            $id_kelas = kelas::where('name', $kelas)->value('id');
 
-            $check_category = category::where('name',$category)->first();
-            if(empty($check_category)){
+            $check_category = category::where('name', $category)->first();
+            if (empty($check_category)) {
                 category::create(
                     [
-                        'name'=>$category
+                        'name' => $category
                     ]
                 );
             }
-            $id_category = category::where('name',$category)->value('id');
+            $id_category = category::where('name', $category)->value('id');
 
-            $check_kontigen = KontigenModel::where('kontigen',$kontigen)->first();
-            if(empty($check_kontigen)){
+            $check_kontigen = KontigenModel::where('kontigen', $kontigen)->first();
+            if (empty($check_kontigen)) {
                 KontigenModel::create(
                     [
-                        'kontigen'=>"$kontigen"
+                        'kontigen' => "$kontigen"
                     ]
                 );
             }
-            $id_kontigen = KontigenModel::where('kontigen',$kontigen)->value('id');
+            $id_kontigen = KontigenModel::where('kontigen', $kontigen)->value('id');
             $tim = [
-                'name'=>$name,
-                'id_kontigen'=>$id_kontigen,
-                'category'=>$id_category,
-                'kelas'=>$id_kelas,
-                'gender'=>$gender,
-                'status'=>'pending'
+                'name' => $name,
+                'id_kontigen' => $id_kontigen,
+                'category' => $id_category,
+                'kelas' => $id_kelas,
+                'gender' => $gender,
+                'status' => 'pending'
             ];
-            
+
             PersertaModel::create($tim);
             // $tim = PersertaModel::where('status', 'biru')->latest()->firstOrNew();
             // $tim->name = $name;
@@ -76,7 +76,7 @@ class Perserta implements ToCollection
 
             // $tim->save();
 
-            
+
         }
     }
 }
